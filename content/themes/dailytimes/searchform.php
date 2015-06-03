64d352fb7c8ed699534a78bac6982632a7b80edc
Clive C. Banditi <clive@cornelo.us>
2015-06-03 10:11:23 +0200

    Removed redundant theme

--- content/themes/dailytimes/searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo esc_url( home_url( '/' ) ); ?>" id="searchform" method="get">
-	<input type="text" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'supernews' ); ?>">
-	<button type="submit" name="submit" id="searchsubmit"><?php _e( 'Search', 'supernews' ); ?></button>
-</form>
