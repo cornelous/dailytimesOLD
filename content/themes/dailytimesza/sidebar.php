0a325ab890c64754ed2369fad42177a2a4c61454
Clive C. Banditi <clive@cornelo.us>
2015-08-20 20:16:36 +0200

    Updated Facebook Comments to v2.3.2

--- content/themes/dailytimesza/sidebar.php
+++ content/themes/dailytimesza/sidebar.php
@@ -11,19 +11,20 @@ if ( in_array( get_theme_mod( 'theme_layout' ), array( '1c' ) ) ) {
 ?>
 
 <div id="secondary" class="widget-area widget-primary sidebar2 column" role="complementary" aria-label="<?php echo esc_attr_x( 'Primary Sidebar', 'Sidebar aria label', 'supernews' ); ?>" <?php hybrid_attr( 'sidebar', 'primary' ); ?>>
-<?php if ( !is_singular( 'post' ) ) : // If viewing a single post page. ?>
-    <div align="center">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- 336x280 -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:336px;height:280px"
-             data-ad-client="ca-pub-8405479344707225"
-             data-ad-slot="1079671995"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
-<?php endif; // End check for type of page being viewed. ?>
+
+    <?php if ( !is_singular( 'post' ) ) : // If viewing a single post page. ?>
+        <div align="center">
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- 336x280 -->
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:336px;height:280px"
+                 data-ad-client="ca-pub-8405479344707225"
+                 data-ad-slot="1079671995"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+    <?php endif; // End check for type of page being viewed. ?>
 
     <?php dynamic_sidebar( 'primary' ); ?>
 </div><!-- #secondary -->
