674fab21f9b0f0c6372ffee91865d323ed8d8cb5
Clive C. Banditi <clive@cornelo.us>
2015-10-07 14:11:28 +0200

    Added newspaper theme and botswana deploy file

---
+++ config/deploy/botswana.rb
@@ -0,0 +1,38 @@
+############################################
+# Setup Server
+############################################
+
+set :stage, :botswana
+set :stage_url, "http://dailytimes.co.bw"
+server "54.68.244.159", user: "ubuntu", roles: %w{web app db}
+
+set :pty, true
+
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: ["publickey"],
+  keys: ["/home/clive/Desktop/.ssh/NewsAfricaTODAY.pem"]
+}
+
+set :deploy_to, "/var/www/html/dailytimes.co.bw"
+
+############################################
+# Setup Git
+############################################
+
+set :branch, "master"
+
+############################################
+# Extra Settings
+############################################
+
+#specify extra ssh options:
+
+#set :ssh_options, {
+#    auth_methods: %w(password),
+#    password: 'password',
+#    user: 'username',
+#}
+
+#specify a specific temp dir if user is jailed to home
+#set :tmp_dir, "/path/to/custom/tmp"
