99ce89fcd599144b990420fe3819f80f6417a101
Clive C. Banditi <clive@cornelo.us>
2016-03-07 22:13:48 +0200

    Updated CleanTalk to v5.36.1

--- content/plugins/cleantalk-spam-protect/inc/cleantalk_nocache.js
+++ content/plugins/cleantalk-spam-protect/inc/cleantalk_nocache.js
@@ -130,4 +130,80 @@ if(ct_nocache_executed==undefined)
 		//alert('set!');
 		sendRequest(ct_ajaxurl+'?'+Math.random(),ct_callback,'action=ct_get_cookie');
 	}
+	
+	if(ct_info_flag)
+	{
+	
+		var cleantalk_user_info={};
+		
+		var cleantalk_screen_info={};
+		for(var prop in screen)
+		{
+			if (navigator[prop] instanceof Object || screen[prop]==='') continue
+			cleantalk_screen_info[prop]=screen[prop];
+		}
+		
+		cleantalk_user_info.screen=cleantalk_screen_info;
+		
+		var cleantalk_plugins=Array();
+		var prev
+		var cnt=0;
+		for(var i=0;i<navigator.plugins.length;i++)
+		{
+			var plugin = navigator.plugins[i];
+			var plugin = plugin.name+" "+(plugin.version || '')
+			if (prev == plugin ) continue;
+			cleantalk_plugins[cnt]=plugin;
+			cnt++;
+			prev = plugin
+		}
+		cleantalk_user_info.plugins=cleantalk_plugins;
+		
+		cleantalk_user_info.timezone_offset = -new Date().getTimezoneOffset()/60;
+		cleantalk_user_info.datetime = Math.round((new Date().getTime())/1000);
+		
+		cleantalk_user_info.browser_x=document.documentElement.clientWidth;
+		cleantalk_user_info.browser_y=document.documentElement.clientHeight;
+		
+		var ua = navigator.userAgent.toLowerCase();
+		var flashInstalled = 0;
+		if (typeof(navigator.plugins)!="undefined"&&typeof(navigator.plugins["Shockwave Flash"])=="object")
+		{
+			flashInstalled = 1;
+		}
+		else if (typeof window.ActiveXObject != "undefined")
+		{
+			try
+			{
+				if (new ActiveXObject("ShockwaveFlash.ShockwaveFlash"))
+				{
+					flashInstalled = 1;
+				}
+			} catch(e) {};
+		};
+		
+		cleantalk_user_info.is_flash=flashInstalled;
+		
+		isVisitedMain=-1;
+		if(location.href=='http://'+location.hostname+'/' || location.href=='https://'+location.hostname+'/')
+		{
+			isVisitedMain=1;
+			setTimeout(function() { document.cookie = "ct_visited_main = 1; path = /;"}, 500);
+		}
+		
+		
+		ct_visited_main = ct_getCookie('ct_visited_main');
+		if(ct_visited_main==undefined && isVisitedMain==-1)
+		{
+			isVisitedMain=0;
+		}
+		else
+		{
+			isVisitedMain=1;
+		}
+		
+		cleantalk_user_info.is_main=isVisitedMain;
+		
+		setTimeout(function() { document.cookie = "ct_user_info = "+escape(JSON.stringify(cleantalk_user_info))+"; path = /;"}, 500);
+	}
 }
